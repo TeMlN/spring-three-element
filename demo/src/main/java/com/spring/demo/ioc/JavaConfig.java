c209a9aaaf6f29727cb51e831520b0bef46527ca
taem1n1 <youman7953@naver.com>
2022-03-27 03:40:11 +0900

    Java file을 이용하여 Bean 직접 등록

--- demo/src/main/java/com/spring/demo/ioc/JavaConfig.java
+++ demo/src/main/java/com/spring/demo/ioc/JavaConfig.java
@@ -1,2 +1,16 @@
-package com.spring.demo.ioc;public class JavaConfig {
+package com.spring.demo.ioc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//Bean 직접 등록
+@Configuration
+public class JavaConfig {
+
+    @Bean
+    public ExampleRepository exampleRepository() {
+        return new ExampleRepository();
+    }
+
+    //이렇게 직접 등록을 해주면 ExampleRepository 에서 @Component 어노테이션이 없어도 IoC 컨테이너에 Bean으로 등록된 것이다
 }
