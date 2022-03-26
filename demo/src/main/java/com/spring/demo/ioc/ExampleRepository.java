21b346ec1674c55811c6820f76530f47189d2c9f
taem1n1 <youman7953@naver.com>
2022-03-27 03:42:53 +0900

    Bean으로 등록

--- demo/src/main/java/com/spring/demo/ioc/ExampleRepository.java
+++ demo/src/main/java/com/spring/demo/ioc/ExampleRepository.java
@@ -1,5 +1,9 @@
 package com.spring.demo.ioc;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ExampleRepository {
     //예시를 이용한 method
     public void save() {
