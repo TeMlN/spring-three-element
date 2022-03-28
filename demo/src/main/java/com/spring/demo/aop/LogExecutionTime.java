f9c264f98d5ff3698a2df3466af1288e03a824ea
taem1n1 <youman7953@naver.com>
2022-03-28 17:44:23 +0900

    사용자 정의 어노테이션

--- demo/src/main/java/com/spring/demo/aop/LogExecutionTime.java
+++ demo/src/main/java/com/spring/demo/aop/LogExecutionTime.java
@@ -1,4 +1,12 @@
 package com.spring.demo.aop;
 
-public @interface LogExcution {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD) //Method 에만 해당 Annotation을 추가 할 수 있게하는 설
+@Retention(RetentionPolicy.RUNTIME )
+public @interface LogExecutionTime  {
+
 }
