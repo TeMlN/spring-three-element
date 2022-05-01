0f40518441c4ff7142693a794cb20b5562e70afb
taem1n1 <youman7953@naver.com>
2022-05-01 16:21:54 +0900

    구현체가 두개일때 케이스

--- src/main/java/com/spring/demo/ioc/example/LegacyMemberServiceImpl.java
+++ src/main/java/com/spring/demo/ioc/example/LegacyMemberServiceImpl.java
@@ -1,2 +1,17 @@
-package com.spring.demo.ioc.example;public class LegacyMemberServiceImpl {
+package com.spring.demo.ioc.example;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LegacyMemberServiceImpl implements MemberService {
+
+    @Override
+    public void signup() {
+        System.out.println("구식 회원가입 방법이에요!");
+    }
+
+    @Override
+    public void login() {
+        System.out.println("구식 로그인 방법이에요!");
+    }
 }
--- src/main/java/com/spring/demo/ioc/example/NewMemberServiceImpl.java
+++ src/main/java/com/spring/demo/ioc/example/NewMemberServiceImpl.java
@@ -1,4 +1,17 @@
-package com.spring.demo.ioc;
+package com.spring.demo.ioc.example;
 
-public class MemberServiceImpl implements {
+import org.springframework.stereotype.Service;
+
+@Service
+public class NewMemberServiceImpl implements MemberService{
+
+    @Override
+    public void signup() {
+        System.out.println("새로운 회원가입 방법이에요!");
+    }
+
+    @Override
+    public void login() {
+        System.out.println("새로운 로그인 방법이에요!");
+    }
 }
