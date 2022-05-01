cdfc169a47922944ca08e8bed91939104e3b6530
taem1n1 <youman7953@naver.com>
2022-05-01 16:22:18 +0900

    기본 구현체

--- src/main/java/com/spring/demo/ioc/example/MemberServiceImpl.java
+++ src/main/java/com/spring/demo/ioc/example/MemberServiceImpl.java
@@ -1,2 +1,16 @@
-package com.spring.demo.ioc.example;public class MemberServiceImpl {
+package com.spring.demo.ioc.example;
+
+import org.springframework.stereotype.Service;
+
+//@Service
+public class MemberServiceImpl implements MemberService {
+    @Override
+    public void signup() {
+        System.out.println("회원가입");
+    }
+
+    @Override
+    public void login() {
+        System.out.println("로그인");
+    }
 }
